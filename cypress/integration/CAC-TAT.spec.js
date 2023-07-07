857f6a78214e98dbab73d08147ba4d095b52601d
amanda.carli <amanda_carli@terceiros.sicredi.com.br>
2023-07-07 15:39:32 -0300

    feat: desafio encontre o gato

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -212,4 +212,21 @@ describe("Central de Atendimento ao Cliente TAT", function () {
       .invoke("val", longTest)
       .should("have.value", longTest);
   });
+
+  it("faz uma requisição HTTP", function () {
+    cy.request(
+      "https://cac-tat.s3.eu-central-1.amazonaws.com/index.html"
+    ).should(function (response) {
+      const { status, statusText, body } = response;
+      expect(status).to.equal(200);
+      expect(statusText).to.equal("OK");
+      expect(body).to.include("CAC TAT");
+    });
+  });
+
+  it("encontra o gato escondido", function () {
+    cy.get("#cat").invoke("show").should("be.visible");
+    cy.get("#title").invoke("text", "CAT TAT");
+    cy.get("#subtitle").invoke("text", "Eu amo gatos");
+  });
 });
