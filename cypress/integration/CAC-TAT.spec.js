6fd54c1633d6f64747ed122f30ff54e942c396b7
amanda.carli <amanda_carli@terceiros.sicredi.com.br>
2023-07-07 15:09:33 -0300

    feat: utilizando loadsh repeat e mais funcoes do invoke

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -101,7 +101,7 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     cy.get(".error").should("not.be.visible");
   });
 
-  it.only("envia o formulário com sucesso usando um comando customizado", function () {
+  it("envia o formulário com sucesso usando um comando customizado", function () {
     cy.clock();
 
     cy.fillMandatoryFieldsAndSubmit(
@@ -186,5 +186,30 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     cy.get("#privacy a").invoke("removeAttr", "target").click();
 
     cy.contains("Talking About Testing").should("be.visible");
+  });
+
+  it("exibe e esconde as mensagens de sucesso e erro usando o .invoke", () => {
+    cy.get(".success")
+      .should("not.be.visible")
+      .invoke("show")
+      .should("be.visible")
+      .and("contain", "Mensagem enviada com sucesso.")
+      .invoke("hide")
+      .should("not.be.visible");
+    cy.get(".error")
+      .should("not.be.visible")
+      .invoke("show")
+      .should("be.visible")
+      .and("contain", "Valide os campos obrigatórios!")
+      .invoke("hide")
+      .should("not.be.visible");
+  });
+
+  it("preenche a area de texto usando o comando invoke", function () {
+    const longTest = Cypress._.repeat("0123456789", 20);
+
+    cy.get("#open-text-area")
+      .invoke("val", longTest)
+      .should("have.value", longTest);
   });
 });
